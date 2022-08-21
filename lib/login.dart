88d4223500b08bc2159e02e88d0ba70bd4236ad0
Rohan Singh <rohansingh.singh70@gmail.com>
2022-08-21 23:55:55 +0530

    Added response for next

--- lib/login.dart
+++ lib/login.dart
@@ -65,6 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: const Text('NEXT'),
                   onPressed: () {
                     // TODO: Show the next page (101)
+                    Navigator.pop(context);
                   },
                 ),
               ],
